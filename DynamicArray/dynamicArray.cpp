58f7ca0c86d0c9c8b4d3a51f854bf324dcb3c7bc
blueskies13 <nicolas-coulombe@hotmail.com>
2015-02-06 10:17:32 -0500

    Lab 2 and 3
    1 Test not working properly

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,53 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(unsigned int _nbElements)
+{
+	capacite = _nbElements;
+	tabElements = new int[capacite];
+
+	for (unsigned int compteur = 0; compteur < 100; compteur++){
+		tabElements[compteur] = 0;
+	}
+}
+
+DynamicArray::~DynamicArray()
+{
+	delete[] tabElements;
+}
+
+void DynamicArray::setElement(unsigned int _index, int _element)
+{
+	if (_index >= capacite){
+		setCapacite(_index + 1);
+	}
+	tabElements[_index] = _element;
+}
+
+int DynamicArray::getElement(unsigned int _index) const
+{
+	if (_index <= capacite){
+		return tabElements[_index];
+	}
+}
+
+int DynamicArray::getCapacite() const
+{
+	return capacite;
+}
+
+void DynamicArray::setCapacite(unsigned int _capacite)
+{
+	if (_capacite <= 0)
+	{
+		int * nouveauTab = new int[_capacite];
+
+		for (unsigned int i = 0; i < min(capacite, _capacite); i++)
+		{
+			nouveauTab[i] = tabElements[i];
+		}
+	}
+	//for (unsigned int i = 0; capacite < _capacite; i++)
+	//{
+
+	//}
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -1,17 +1,25 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
 
 class DynamicArray
 {
 
 public:
-
-
+	DynamicArray(unsigned int _nbElements = 100);
+	~DynamicArray();
+	void setElement(unsigned int _index, int _valeur);
+	int getElement(unsigned int _index) const;
+	int getCapacite() const;
+	void setCapacite(unsigned int _capacite);
 
 private:
-
-
+	int* tabElements;
+	unsigned int capacite;
 };
 
 
